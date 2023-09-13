30d3179375baca540a9ae203feb7c24ac345da78
Math33t3 <math33t3@stud.kea.dk>
2023-09-13 16:22:54 +0200

    first commit

---
+++ public/admin.js
@@ -0,0 +1,30 @@
+// public/admin.js
+'use strict';
+const socket = io();
+const peer = new RTCPeerConnection();
+
+const video = document.getElementById('client-screen');
+peer.addEventListener('track', (track) => {
+  video.srcObject = track.streams[0];
+});
+
+socket.on('offer', async (clientSDP) => {
+  await peer.setRemoteDescription(clientSDP);
+
+  const sdp = await peer.createAnswer();
+  await peer.setLocalDescription(sdp);
+
+  if(confirm("Press to accept the screenshare!")) {
+    socket.emit('answer', peer.localDescription);
+  };
+  
+});
+
+peer.addEventListener('icecandidate', (event) => {
+  if (event.candidate) {
+    socket.emit('icecandidate', event.candidate);
+  }
+});
+socket.on('icecandidate', async (candidate) => {
+  await peer.addIceCandidate(new RTCIceCandidate(candidate));
+});
